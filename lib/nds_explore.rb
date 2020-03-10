5bda3e24e0731c22b54a2237e88306d1df391774
Paul Ralyea <047par@gmail.com>
2020-03-10 21:49:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,19 +9,19 @@ end
 
 def print_first_directors_movie_titles
   row_index = 0
-while row_index < directors_db.length do
-  puts "Row #{row_index} has #{directors_db[row_index]} columns"
+while row_index < nds.length do
+  puts "Row #{row_index} has #{nds[row_index]} columns"
  
   column_index = 0
-  while column_index < directors_db[row_index].length do
+  while column_index < nds[row_index].length do
     coord = "#{row_index}, #{column_index}"
-    inner_len = directors_db[row_index][column_index].length
+    inner_len = nds[row_index][column_index].length
     # Remember \t is a TAB character for indentation
-    puts "\tCoordinate [#{coord}] points to an #{directors_db[row_index][column_index].class} of length #{inner_len}"
+    puts "\tCoordinate [#{coord}] points to an #{nds[row_index][column_index].class} of length #{inner_len}"
  
     inner_index = 0
     while inner_index < inner_len do
-      puts "\t\t (#{coord}, #{inner_index}) is: #{directors_db[row_index][column_index][inner_index]}"
+      puts "\t\t (#{coord}, #{inner_index}) is: #{nds[row_index][column_index][inner_index]}"
       inner_index += 1
     end
  
