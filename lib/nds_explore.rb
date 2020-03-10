b3056456a5a8ac5f693d957c011d63ff7b0368f1
Paul Ralyea <047par@gmail.com>
2020-03-10 22:01:08 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -17,27 +17,4 @@ def print_first_directors_movie_titles
  end
  print_first_directors_movie_titles
 
-def print_first_directors_movie_titles
-  row_index = 0
-while row_index < nds.length do
-  puts "Row #{row_index} has #{nds[row_index]} columns"
- 
-  column_index = 0
-  while column_index < nds[row_index].length do
-    coord = "#{row_index}, #{column_index}"
-    inner_len = nds[row_index][column_index].length
-    # Remember \t is a TAB character for indentation
-    puts "\tCoordinate [#{coord}] points to an #{nds[row_index][column_index].class} of length #{inner_len}"
- 
-    inner_index = 0
-    while inner_index < inner_len do
-      puts "\t\t (#{coord}, #{inner_index}) is: #{nds[row_index][column_index][inner_index]}"
-      inner_index += 1
-    end
- 
-    column_index += 1
-  end
- 
-  row_index += 1
-end
-end
+
